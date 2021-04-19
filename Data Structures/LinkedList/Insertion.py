0c900c80064bd842c37e4142832fd6704a806455
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-19 10:19:59 +0530

    Logic for inserting between the nodes added

--- Data Structures/LinkedList/Insertion.py
+++ Data Structures/LinkedList/Insertion.py
@@ -19,4 +19,5 @@ if __name__=='__main__':
     print(' ')
 
     # inserting in between
+    llist.insertBtw(second, 8)
     llist.printList()
--- Data Structures/LinkedList/LinkedList.py
+++ Data Structures/LinkedList/LinkedList.py
@@ -27,17 +27,3 @@ class LinkedList:
     while(temp):
       print(temp.data)
       temp = temp.next
-
-if __name__=='__main__':
-  
-    # Start with the empty list
-    llist = LinkedList()
-  
-    llist.head = Node(1)
-    second = Node(2)
-    third = Node(3)
-  
-    llist.head.next = second; # Link first node with second
-    second.next = third; # Link second node with the third node
-  
-    llist.printList()
