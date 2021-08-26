df87d76aebd7388fdd0721bd64fec30e0215b7b1
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-26 23:51:28 +0530

    update: updated the file for PostionDeletiom

--- DataStructures/LinkedList/PositionDeletion.py
+++ DataStructures/LinkedList/PositionDeletion.py
@@ -1,21 +1,20 @@
-from LinkedList import LinkedList,Node
+from Datastructures.LinkedList.LinkedListConstruct import LinkedList, Node
 
-if __name__ == '__main__':
-  llist = LinkedList()
-  llist.head = Node(1)
-  second = Node(2)
-  third = Node(3)
-  fourth = Node(4)
+llist = LinkedList()
+llist.head = Node(1)
+second = Node(2)
+third = Node(3)
+fourth = Node(4)
 
-  llist.head.next = second
-  second.next = third
-  third.next = fourth
+llist.head.next = second
+second.next = third
+third.next = fourth
 
-  llist.printList()
-  print("*" * 30)
-  print(' ')
+llist.printList()
+print("*" * 30)
+print(' ')
 
-  llist.deletePosNode(2)
-  llist.printList()
-  print("*" * 30)
-  print(' ')
+llist.deletePosNode(2)
+llist.printList()
+print("*" * 30)
+print(' ')
