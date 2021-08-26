e89f87fa8da178500882309687d248c8b668f737
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-26 23:52:06 +0530

    update: updated the data for LengthCount

--- DataStructures/LinkedList/LengthCount.py
+++ DataStructures/LinkedList/LengthCount.py
@@ -1,4 +1,4 @@
-from LinkedListConstruct import Node, createdLinkedList
+from Datastructures.LinkedList.LinkedListConstruct import Node, createdLinkedList
 
 list = createdLinkedList()
 print(list.printLength())
