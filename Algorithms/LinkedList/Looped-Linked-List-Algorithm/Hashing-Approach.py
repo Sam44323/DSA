66465d7d4a11cd3c4d3f49d2828784ae570b9e17
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-28 21:37:56 +0530

    update: updated the loops

--- Algorithms/LinkedList/Looped-Linked-List-Algorithm/Hashing-Approach.py
+++ Algorithms/LinkedList/Looped-Linked-List-Algorithm/Hashing-Approach.py
@@ -1,5 +1,5 @@
 from Datastructures.LinkedList.LinkedListConstruct import createdLinkedList
 
 list = createdLinkedList()
-list.head.next.next.next = list.head
+list.head.next.next = list.head
 print(list.hashingCheckLoop())
