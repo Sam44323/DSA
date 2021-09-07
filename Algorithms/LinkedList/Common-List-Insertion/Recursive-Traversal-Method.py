0bf3eeaf6e86facc79d33690244ff6167ea14f73
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-08 00:08:13 +0530

    add: added file for recursive traversal method for intersection

---
+++ Algorithms/LinkedList/Common-List-Insertion/Recursive-Traversal-Method.py
@@ -0,0 +1,24 @@
+from Datastructures.LinkedList.LinkedListConstruct import LinkedList, Node, commonListIntersectionTraversal
+
+listFirst = LinkedList()  # creating an empty list
+listFirst.head = Node(1)
+second = Node(2)
+third = Node(3)
+fourth = Node(4)
+fifth = Node(5)
+listFirst.head.next = second
+second.next = third
+third.next = fourth
+fourth.next = fifth
+listSecond = LinkedList()  # creating an empty list
+listSecond.head = Node(1)
+second = Node(2)
+third = Node(3)
+listSecond.head.next = second
+second.next = third
+list = commonListIntersectionTraversal(
+    listFirst, listSecond)
+
+list = list.head.next
+while list != None:
+    print(list.data)
