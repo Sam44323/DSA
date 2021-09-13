d5f201532dd3b90f840cb30abbebc1ac32bedd2b
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-13 22:14:32 +0530

    add: added file for node count difference for intersection encounter

---
+++ Algorithms/LinkedList/Intersection-Point/node-count-difference.py
@@ -0,0 +1,14 @@
+from Datastructures.LinkedList.LinkedListConstruct import LinkedList, Node, intersectionPointLoopMethod
+
+common = Node(8)
+
+listFirst = LinkedList()  # creating an empty list
+listFirst.head = Node(3)
+listFirst.head.next = Node(6)
+listFirst.head.next.next = Node(9)
+listFirst.head.next.next.next = common
+listFirst.head.next.next.next.next = Node(30)
+listSecond = LinkedList()
+listSecond.head = Node(10)
+listSecond.head.next = common
+listSecond.head.next.next = Node(100)
